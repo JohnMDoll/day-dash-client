d007e91c3380a5f70f938de97fa2d4059fa4417e
JohnMDoll <johnmdoll3@gmail.com>
2023-03-17 09:56:46 -0500

    further url modifications for gh-pages

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -6,10 +6,10 @@ import { Profile } from "../profile/Profile";
 export const ApplicationViews = () => {
 	return (
     <Routes>
-      <Route path="/" element={<Navigate to="/home" />} />
-      <Route path="/home" element={<Home />} />
-      <Route path={`/friend/:id`} element={<FriendProfile />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route path="./" element={<Navigate to="/home" />} />
+      <Route path="./home" element={<Home />} />
+      <Route path={`./friend/:id`} element={<FriendProfile />} />
+      <Route path="./profile" element={<Profile />} />
     </Routes>
   );
 }
