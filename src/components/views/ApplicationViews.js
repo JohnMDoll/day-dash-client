6ab2c9a157ca6c978fa26d6e51a01aeeef2b0c18
JohnMDoll <johnmdoll3@gmail.com>
2023-03-17 13:21:32 -0500

    went back to browserRouter, added Outlet to ApplicationView routes

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,17 +1,20 @@
 import { Outlet, Route, Routes } from "react-router-dom"
-import { FriendProfile } from "../friendpage/FriendProfile";
-import { Home } from "../home/home";
-import { Profile } from "../profile/Profile";
+import { FriendProfile } from "../friendpage/FriendProfile"
+import { Home } from "../home/home"
+import { Profile } from "../profile/Profile"
 
 export const ApplicationViews = () => {
   return (
     <Routes>
-      <Route path="/" element={<Outlet /> }>
-        
-      <Route path="/home" element={<Home />} />
-      <Route path={`/friend/:id`} element={<FriendProfile />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route path="/" element={
+        <>
+          <Outlet />
+        </>
+      }>
+        <Route path="/home" element={<Home />} />
+        <Route path={`/friend/:id`} element={<FriendProfile />} />
+        <Route path="/profile" element={<Profile />} />
       </Route>
-    </Routes>
-  );
+    </Routes >
+  )
 }
