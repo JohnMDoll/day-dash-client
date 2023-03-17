9c3cd23c8b669b3407ee47072ca384171375a219
JohnMDoll <johnmdoll3@gmail.com>
2023-03-17 10:22:51 -0500

    changed browserRouter to HashRouter in index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { DayDash } from './DayDash';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<BrowserRouter basename='/day-dash-client'>
+	<HashRouter basename='/day-dash-client'>
 		<DayDash />
-	</BrowserRouter>
+	</HashRouter>
 );
 
