530c72fbc8a8e3136cfd5060c69c2ed58e83757b
JohnMDoll <johnmdoll3@gmail.com>
2023-03-17 09:12:11 -0500

    further url modifications for gh-pages

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -21,7 +21,7 @@ export const Login = () => {
     const submitHandler = async (e) => {
         e.preventDefault()
         await (register? registerUser(newUser) : loginUser(user))
-        if (localStorage.getItem("dd_user")) {navigate("/home")}
+        if (localStorage.getItem("dd_user")) {navigate("/day-dash-client//home")}
     }
 
     return <main className="container--login">
